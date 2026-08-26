a1d496453701253ecabc4789eccfed7f6d09b280
Megha Naik <159499761+Megznaik@users.noreply.github.com>
2026-08-26 16:28:27 -0400

    Time: 26 ms (26.66%) | Memory: 23.6 MB (18.61%) - LeetSync

--- 695-max-area-of-island/max-area-of-island.py
+++ 695-max-area-of-island/max-area-of-island.py
@@ -7,7 +7,7 @@ class Solution:
             if( r < 0 or c < 0 or r == ROWS or c == COLS or grid[r][c] == 0 or (r,c) in visit):
                 return 0
             
-            grid[r][c] = 0
+            visit.add((r,c))
             return ( 1 + dfs(r+1, c) + dfs(r-1, c) + dfs(r,c+1) + dfs(r,c-1))
         
         area = 0
