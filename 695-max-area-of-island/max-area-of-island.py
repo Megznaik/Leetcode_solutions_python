b769566fa74b4d7257edbf1d7fd91a402f7b349a
Megha Naik <159499761+Megznaik@users.noreply.github.com>
2026-08-26 14:46:57 -0400

    Time: 19 ms (61.76%) | Memory: 20.3 MB (65.91%) - LeetSync

--- 695-max-area-of-island/max-area-of-island.py
+++ 695-max-area-of-island/max-area-of-island.py
@@ -3,18 +3,17 @@ class Solution:
         ROWS, COLS = len(grid), len(grid[0])
         visit = set()
 
-        def dfs(r, c):
-            if (r < 0 or c < 0 or r == ROWS or c == COLS or grid[r][c] == 0 or (r,c) in visit):
+        def dfs(r,c):
+            if( r < 0 or c < 0 or r == ROWS or c == COLS or grid[r][c] == 0 or (r,c) in visit):
                 return 0
-            visit.add((r,c))
-            return (1 + dfs(r+1, c) +
-                        dfs(r-1, c) +
-                        dfs(r, c+1) +
-                        dfs(r, c-1))
+            
+            grid[r][c] = 0
+            return ( 1 + dfs(r+1, c) + dfs(r-1, c) + dfs(r,c+1) + dfs(r,c-1))
+        
         area = 0
         for r in range(ROWS):
             for c in range(COLS):
                 area = max(area, dfs(r,c))
+        
         return area
         
-
